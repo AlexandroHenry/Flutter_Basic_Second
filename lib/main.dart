1a90a07045b9535212df7889a78a754d834a0f2e
Seungchul Ha <seungchulha@Seungchuls-MacBook-Pro.local>
2023-05-18 15:07:03 +0900

    List View

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:second_app/onboarding.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,42 +10,185 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: OnBoardingPage(),
+      home: ListViewPage(),
     );
   }
 }
 
-class MyPage extends StatelessWidget {
-  const MyPage({super.key});
+class ListViewPage extends StatefulWidget {
+  const ListViewPage({super.key});
+
+  @override
+  State<ListViewPage> createState() => _ListViewPageState();
+}
+
+class _ListViewPageState extends State<ListViewPage> {
+  var titleList = [
+    'Dentist',
+    'Pharmacist',
+    'Teacher',
+    'IT Manager',
+    'Account',
+    'Lawyer',
+    'Hairdresser',
+    'Physician',
+    'Web Developer',
+    'Medical Secretary'
+  ];
+
+  var imageList = [
+    'assets/images/1.png',
+    'assets/images/2.png',
+    'assets/images/3.png',
+    'assets/images/4.png',
+    'assets/images/5.png',
+    'assets/images/6.png',
+    'assets/images/7.png',
+    'assets/images/8.png',
+    'assets/images/9.png',
+    'assets/images/10.png',
+  ];
+
+  var description = [
+    '1. There are different types of careers you can pursue in your life. Which on will it be?',
+    '2. There are different types of careers you can pursue in your life. Which on will it be?',
+    '3. There are different types of careers you can pursue in your life. Which on will it be?',
+    '4. There are different types of careers you can pursue in your life. Which on will it be?',
+    '5. There are different types of careers you can pursue in your life. Which on will it be?',
+    '6. There are different types of careers you can pursue in your life. Which on will it be?',
+    '7. There are different types of careers you can pursue in your life. Which on will it be?',
+    '8. There are different types of careers you can pursue in your life. Which on will it be?',
+    '9. There are different types of careers you can pursue in your life. Which on will it be?',
+    '10. There are different types of careers you can pursue in your life. Which on will it be?',
+  ];
+
+  void showPopup(context, title, image, description) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.7,
+            height: 380,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
+            ),
+            child: Column(
+              children: [
+                ClipRect(
+                  child: Image.asset(
+                    image,
+                    width: 200,
+                    height: 200,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Text(
+                    description,
+                    maxLines: 3,
+                    style: const TextStyle(
+                      fontSize: 15,
+                      color: Colors.orange,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                ElevatedButton.icon(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(Icons.close),
+                  label: const Text('Close'),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width * 0.6;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Main Page'),
+        title: const Text(
+          'List View',
+          style: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+        backgroundColor: Colors.white,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Main Screen',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 25,
+      body: ListView.builder(
+        itemCount: titleList.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              debugPrint(titleList[index]);
+              showPopup(
+                context,
+                titleList[index],
+                imageList[index],
+                description[index],
+              );
+            },
+            child: Card(
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Image.asset(imageList[index]),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Text(
+                          titleList[index],
+                          style: const TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.red,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        SizedBox(
+                          width: width,
+                          child: Text(
+                            description[index],
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.grey.shade500,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
               ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(
-                      builder: (context) => const OnBoardingPage()),
-                );
-              },
-              child: const Text('Go to Onboarding Screen'),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
